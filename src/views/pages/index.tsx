1df926eaeb7d443cb650152e6583b81977243609
“JunSecondStart” <junsama.silva.dodoria@gmail.com>
2023-12-23 19:16:48 +0900

    routing for http Changes to be committed: modified:   build/asset-manifest.json modified:   build/index.html new file:   build/static/css/main.cacb7682.css new file:   build/static/css/main.cacb7682.css.map deleted:    build/static/css/main.fb3d58b0.css deleted:    build/static/css/main.fb3d58b0.css.map new file:   build/static/js/main.867a18ea.js renamed:    build/static/js/main.c757379c.js.LICENSE.txt -> build/static/js/main.867a18ea.js.LICENSE.txt new file:   build/static/js/main.867a18ea.js.map deleted:    build/static/js/main.c757379c.js deleted:    build/static/js/main.c757379c.js.map modified:   src/views/pages/index.tsx

--- src/views/pages/index.tsx
+++ src/views/pages/index.tsx
@@ -11,13 +11,20 @@ import Coding from "../components/unique/Coding/Coding";
 const Pages: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} />
       <Route path="/coding" element={<Coding />} />
       <Route path="/plans" element={<Plans />} />
       <Route path="/kintone" element={<Kintone />} />
       <Route path="/slack" element={<Slack />} />
       <Route path="/servermanagement" element={<Servermanagement />} />
-      <Route path="/tips" element={<Tips />} />
+      <Route path="/tips" element={<Tips />} /> */}
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/" element={<Home />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/coding" element={<Coding />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/plans" element={<Plans />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/kintone" element={<Kintone />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/slack" element={<Slack />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/servermanagement" element={<Servermanagement />} />
+      <Route path="https://junjunworks.hippy.jp/task_management_for_me/build/index.html/tips" element={<Tips />} />
     </Routes>
   );
 };
