f80ddc38e074f30ebc59fe0e6158ba8ca0c4f222
“JunSecondStart” <junsama.silva.dodoria@gmail.com>
2023-12-30 10:43:18 +0900

    widthened click space on button On branch feature001 Your branch is up to date with 'origin/feature001'.
    Changes to be committed:
    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,12 +31,12 @@ const App:React.FC = () => {
         </div>
         <section className="bg-gray-50">
           <ul className="flex flex-row">
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-green-50 text-green-300"><Link to="/coding" onClick={()=>setPageTitle({selectTopic: "Coding"})}>Coding</Link></li>
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-red-50 text-red-300"><Link to="/plans" onClick={()=>setPageTitle({selectTopic: "Plans"})}>Plans</Link></li>
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-cyan-50 text-cyan-300"><Link to="/kintone" onClick={()=>setPageTitle({selectTopic: "Kintone"})}>Kintone</Link></li>
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-pink-50 text-pink-300"><Link to="/slack" onClick={()=>setPageTitle({selectTopic: "Slack"})}>Slack</Link></li>
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-violet-50 text-violet-300"><Link to="/servermanagement" onClick={()=>setPageTitle({selectTopic: "Servermanagement"})}>Servermanagement</Link></li>
-            <li className="basis-1/6 text-2xl text-center py-4 m-0 hover:bg-yellow-50 text-yellow-300"><Link to="/tips" onClick={()=>setPageTitle({selectTopic: "Tips"})}>Tips</Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-green-50 text-green-300"><Link to="/coding" onClick={()=>setPageTitle({selectTopic: "Coding"})}><p className="py-4 w-xl h-16">Coding</p></Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-red-50 text-red-300"><Link to="/plans" onClick={()=>setPageTitle({selectTopic: "Plans"})}><p className="py-4 w-xl h-16">Plans</p></Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-cyan-50 text-cyan-300"><Link to="/kintone" onClick={()=>setPageTitle({selectTopic: "Kintone"})}><p className="py-4 w-xl h-16">Kintone</p></Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-pink-50 text-pink-300"><Link to="/slack" onClick={()=>setPageTitle({selectTopic: "Slack"})}><p className="py-4 w-xl h-16">Slack</p></Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-violet-50 text-violet-300"><Link to="/servermanagement" onClick={()=>setPageTitle({selectTopic: "Servermanagement"})}><p className="py-4 w-xl h-16">Servermanagement</p></Link></li>
+            <li className="basis-1/6 text-2xl text-center m-0 hover:bg-yellow-50 text-yellow-300"><Link to="/tips" onClick={()=>setPageTitle({selectTopic: "Tips"})}><p className="py-4 w-xl h-16">Tips</p></Link></li>
           </ul>
         </section>
         <section>
